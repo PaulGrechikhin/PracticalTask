fec33cc43a063aed8bf1ae158d78327d570ad9e9
Paul Gre <pgrechihin@mail.ru>
2023-02-11 19:13:40 +0300

    PT_2

---
+++ PT_2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+
+//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.Write("Введите первое число ");
+string text1 = Console.ReadLine();
+Console.Write("Введите второе число ");
+string text2 = Console.ReadLine();
+double a = double.Parse(text1);
+double b = double.Parse(text2);
+
+if(a>b)
+{
+    Console.Write(a + " > " + b);
+}
+else
+{
+    Console.Write(a + " < " + b);
+}
