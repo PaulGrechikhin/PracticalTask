5c113d382003cc9d311dac3b660c1ed752c997db
Paul Gre <pgrechihin@mail.ru>
2023-02-11 21:04:34 +0300

    PT_6

---
+++ PT_6/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+
+//Напишите программу, которая на вход принимает число и выдаёт,
+// является ли число чётным (делится ли оно на два без остатка).
+
+int number;
+Console.Write("Введите натурольное число: ");
+int.TryParse(Console.ReadLine()!, out number);
+
+int remains = number % 2;
+
+if(remains == 0 )
+{
+    Console.Write("Число четное");
+}
+else
+{
+    Console.Write("Число нечетное");
+}
