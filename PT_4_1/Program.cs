c197f63d5c77b63e9047b4608bdfd9e19e9381d2
Paul Gre <pgrechihin@mail.ru>
2023-02-12 09:26:21 +0300

    pt_4_1

---
+++ PT_4_1/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Clear();
+
+//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+double a, b, c;
+
+Console.Write("Введите первое число: ");
+double.TryParse(Console.ReadLine()!, out a);
+Console.Write("Введите первое число: ");
+double.TryParse(Console.ReadLine()!, out b);
+Console.Write("Введите первое число: ");
+double.TryParse(Console.ReadLine()!, out c);
+ 
+double max = a;
+
+if(a > b){}
+else
+    max = b;
+
+if(b > c){}
+else
+    max = c;
+
+Console.Write($"max = {max}");
